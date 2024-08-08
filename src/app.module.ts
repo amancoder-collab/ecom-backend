362a180618a0887f1ea2a36a69aa1ebfc729b75d
rahul7840 <coading2020@gmail.com>
2024-08-08 18:57:17 +0530

    add: common module and pritter & eslint added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { HttpExceptionFilter } from './common/filters/http.exception.filter';
+import { AllExceptionsFilter } from './common/filters/all.exception.filter';
+import { ResponseInterceptor } from './common/interceptor/response.interceptor';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [],
+    controllers: [AppController],
+    providers: [
+        { provide: APP_INTERCEPTOR, useClass: ResponseInterceptor },
+        { provide: APP_FILTER, useClass: AllExceptionsFilter },
+        { provide: APP_FILTER, useClass: HttpExceptionFilter },
+        { provide: APP_PIPE, useClass: ValidationPipe },
+        AppService,
+    ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        allowedHeaders:
+            'Origin,X-Requested-With,Content-Type,Accept,Authorization,authorization,X-Forwarded-for,External_Network,external_network',
+        credentials: true,
+    });
+
+    const config = new DocumentBuilder()
+        .addBearerAuth()
+        .setTitle('E-commerce')
+        .setDescription('Here is the API for E-commerce backend')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    await app.listen(4044);
+    console.log('server is running 4044...');
 }
 bootstrap();
