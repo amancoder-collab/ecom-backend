e93b3b58fef7931e20af337c4c7713e11f8f871e
AmanTheGreatCoder <amanwadhwaniya159@gmail.com>
2024-11-23 14:10:55 +0530

    changed pm2

--- pm2.config.js
+++ pm2.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "my-app",
       script: "./dist/src/main.js",
-      instances: "max",
+      instances: "1",
       exec_mode: "cluster",
       autorestart: true,
       watch: false,
